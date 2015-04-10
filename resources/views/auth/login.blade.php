8b7761520d83b3a4906b3bab5a9784555f9c201f
Miguel Mamani <djmiguelarango@gmail.com>
2015-04-09 22:34:51 -0400

    Blade - Html - Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,14 +24,16 @@
 						<div class="form-group">
 							<label class="col-md-4 control-label">E-Mail Address</label>
 							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                {!! Form::text('email', null, ['class' => 'form-control', 'type' => 'email']) !!}
+								{{--<input type="email" class="form-control" name="email" value="{{ old('email') }}">--}}
 							</div>
 						</div>
 
 						<div class="form-group">
 							<label class="col-md-4 control-label">Password</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+                                {!! Form::password('password', ['class' => 'form-control']) !!}
+								{{--<input type="password" class="form-control" name="password">--}}
 							</div>
 						</div>
 
@@ -39,7 +41,8 @@
 							<div class="col-md-6 col-md-offset-4">
 								<div class="checkbox">
 									<label>
-										<input type="checkbox" name="remember"> Remember Me
+                                        {!! Form::checkbox('remember', '') !!} Remember Me
+										{{--<input type="checkbox" name="remember"> Remember Me--}}
 									</label>
 								</div>
 							</div>
